cf47c425cc264acc2d0f2078c9690b964682fc7b
rudalkunwar <ruzalkunwar@gmail.com>
2026-06-01 00:34:34 +0545

    feat : basic packages and fix test file

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,15 @@
 <?php
 
-test('the application returns a successful response', function () {
-    $response = $this->get('/');
+use function Pest\Laravel\get;
+
+test('the health endpoint returns a successful response', function () {
+    $response = get('/up');
 
     $response->assertStatus(200);
+});
+
+test('api endpoints return json', function () {
+    $response = get('/api/non-existent', ['Accept' => 'application/json']);
+
+    $response->assertStatus(404);
 });
